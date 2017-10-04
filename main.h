86c608123b2f92a0b6b67f179b7576f6d3e8dfdf
Andreas Louv <andreas@louv.dk>
2017-10-04 14:43:09 +0200

    Updated Usage

--- main.h
+++ main.h
@@ -2,11 +2,11 @@
 #define _H_MAIN_ 1
 
 #define PROGRAM_NAME	"zs"
-#define	PROGRAM_VERSION	"0.7"
-#define	PROGRAM_USAGE	"Usage " PROGRAM_NAME " { "			\
-	"{ -s=server | -u=user | -p=password | -c=file }...\n"		\
-	"\t   { -S=server | -U=user | -P=password | -C=file }...\n"	\
-	"\t   { LIB/OBJ [ *TYPE ] }...\n"				\
+#define	PROGRAM_VERSION	"0.7.1"
+#define	PROGRAM_USAGE	"Usage " PROGRAM_NAME " { "			      \
+	"{ -s=server | -u=user | -p=password | -j=file | -c=file }...\n"      \
+	"\t   { -S=server | -U=user | -P=password | -J=file | -C=file }...\n" \
+	"\t   { LIB/OBJ [ *TYPE ] }...\n"				      \
 	"\t   | --help | --version }\n"
 
 #endif
